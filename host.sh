6cd2ae16ad25d3652f63b7b4874a7117e1320ea9
Walter Sonius <walterav1984@gmail.com>
2018-12-05 14:20:25 +0100

    debian & ubuntu release/codename support
    Allowing debian 8 jessie and ubuntu 18.04 bionic as a release version.

--- host.sh
+++ host.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# host.sh 'distro' 'architecture' 'imgsize in megabytes numbers'
+# host.sh 'action' 'distro' 'codename' 'architecture' 'imgsize in gigabytes numbers'
 #
-#Its pretty obvious what happens here, but it installs qemu, downloads an iso 
+#Its pretty obvious what happens here, it installs qemu, downloads an iso 
 #creates a xGB diskimage for the virtualmachine starts the virtualmachine with
 #telnet capabilities and let 'host2guest.sh' feed/automate the whole process.
 
@@ -12,14 +12,17 @@ XACT=$1
 #second argument defines distro
 DISTRO=$2
 
-#third argument defines architecture
-IARCH=$3
+#third argument defines codename/release
+CNAME=$3
 
-#fourth argument defines fixed img size of 1GB / 2GB
+#fouth argument defines architecture
+IARCH=$4
+
+#fifth argument defines fixed img size of 1GB / 2GB
 #2gb or larger installs default utils during install and raspbian-litefull vs slim package selection?
-#2gb larget boot volume
-DSIZE=$4
-#echo 50mb smaller?
+#2gb larger boot volume
+DSIZE=$5
+#echo 50MB smaller?
 case $DSIZE in
 1GB)
 ISIZE=976
@@ -30,7 +33,7 @@ ISIZE=1950
 esac
 
 #telnetport based on architecture allows i386/amd64 img creation at same time
-TPORT=$(case $3 in
+TPORT=$(case $IARCH in
 i386)
 echo "9332"
 ;;
@@ -55,16 +58,30 @@ esac
 #distro&arch dependent variables
 case $DISTRO in
 debian)
-QCMPAYLOAD=qcmpayloadd.txt
+    case $CNAME in
+        jessie)
+        QCMPAYLOAD=qcmpayloadd8.txt
+        ;;
+        stretch)
+        QCMPAYLOAD=qcmpayloadd.txt
+        ;;
+    esac
 OVBOSTYPE="Debian$OVBARCH"
 ;;
 ubuntu)
-QCMPAYLOAD=qcmpayloadu.txt
+    case $CNAME in
+        xenial)
+        QCMPAYLOAD=qcmpayloadu.txt
+        ;;
+        bionic)
+        QCMPAYLOAD=qcmpayloadu1804.txt
+        ;;
+    esac
 OVBOSTYPE="Ubuntu$OVBARCH"
 ;;
 esac
 
-DISO="isos/$DISTRO-$IARCH.iso"
+DISO="isos/$DISTRO-$CNAME-$IARCH.iso"
 
 function prepare {
 sudo apt-get update
@@ -73,42 +90,50 @@ mkdir isos
 cd isos
 DISTRO=debian
 IARCH=i386
-wget https://caesar.ftp.acc.umu.se/debian-cd/current/$IARCH/iso-cd/debian-9.5.0-$IARCH-netinst.iso -O $DISTRO-$IARCH.iso
+wget https://cdimage.debian.org/cdimage/archive/8.11.0/$IARCH/iso-cd/debian-8.11.0-$IARCH-netinst.iso -O $DISTRO-jessie-$IARCH.iso
 IARCH=amd64
-wget https://caesar.ftp.acc.umu.se/debian-cd/current/$IARCH/iso-cd/debian-9.5.0-$IARCH-netinst.iso -O $DISTRO-$IARCH.iso
+wget https://cdimage.debian.org/cdimage/archive/8.11.0/$IARCH/iso-cd/debian-8.11.0-$IARCH-netinst.iso -O $DISTRO-jessie-$IARCH.iso
+IARCH=i386
+wget https://cdimage.debian.org/cdimage/archive/9.5.0/$IARCH/iso-cd/debian-9.5.0-$IARCH-netinst.iso -O $DISTRO-stretch-$IARCH.iso
+IARCH=amd64
+wget https://cdimage.debian.org/cdimage/archive/9.5.0/$IARCH/iso-cd/debian-9.5.0-$IARCH-netinst.iso -O $DISTRO-stretch-$IARCH.iso
 DISTRO=ubuntu
 IARCH=i386
-wget http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-$IARCH/current/images/netboot/mini.iso -O $DISTRO-$IARCH.iso
+wget http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-$IARCH/current/images/netboot/mini.iso -O $DISTRO-xenial-$IARCH.iso
 IARCH=amd64
-wget http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-$IARCH/current/images/netboot/mini.iso -O $DISTRO-$IARCH.iso
+wget http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-$IARCH/current/images/netboot/mini.iso -O $DISTRO-xenial-$IARCH.iso
+IARCH=i386
+wget http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-$IARCH/current/images/netboot/mini.iso -O $DISTRO-bionic-$IARCH.iso
+IARCH=amd64
+wget http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-$IARCH/current/images/netboot/mini.iso -O $DISTRO-bionic-$IARCH.iso
 cd ..
 }
 
 function create {
 #create system
-qemu-img create -f raw disk-$DISTRO-$IARCH-$DSIZE.img $ISIZE'M'
-sudo qemu-system-$QARCH -enable-kvm -drive format=raw,file=disk-$DISTRO-$IARCH-$DSIZE.img -cdrom $DISO -boot d -m 512 -monitor telnet:localhost:$TPORT,server,nowait & #-device usb-ehci,id=ehci -device usb-host,id=asix,bus=ehci.0,vendorid=0x0b95,productid=0x7720 &
+qemu-img create -f raw disk-$DISTRO-$CNAME-$IARCH-$DSIZE.img $ISIZE'M'
+sudo qemu-system-$QARCH -enable-kvm -drive format=raw,file=disk-$DISTRO-$CNAME-$IARCH-$DSIZE.img -cdrom $DISO -boot d -m 512 -monitor telnet:localhost:$TPORT,server,nowait & #-device usb-ehci,id=ehci -device usb-host,id=asix,bus=ehci.0,vendorid=0x0b95,productid=0x7720 &
 sleep 10
 ./host2guest.sh $QCMPAYLOAD $TPORT $IARCH $DSIZE 
 }
 
 function modify {
 #boot current image
-sudo qemu-system-$QARCH -enable-kvm -drive format=raw,file=disk-$DISTRO-$IARCH-$DSIZE.img -cdrom $DISO -boot c -m 512 -monitor telnet:localhost:$TPORT,server,nowait & #-device usb-ehci,id=ehci -device usb-host,id=asix,bus=ehci.0,vendorid=0x0b95,productid=0x7720 &
+sudo qemu-system-$QARCH -enable-kvm -drive format=raw,file=disk-$DISTRO-$CNAME-$IARCH-$DSIZE.img -cdrom $DISO -boot c -m 512 -monitor telnet:localhost:$TPORT,server,nowait & #-device usb-ehci,id=ehci -device usb-host,id=asix,bus=ehci.0,vendorid=0x0b95,productid=0x7720 &
 }
 
 function convertovb {
 #install target guestadditions
 QCMPAYLOAD=qcmpayload-covb.txt
-sudo qemu-system-$QARCH -enable-kvm -drive format=raw,file=disk-$DISTRO-$IARCH-$DSIZE.img -cdrom $DISO -boot c -m 512 -monitor telnet:localhost:$TPORT,server,nowait & #-device usb-ehci,id=ehci -device usb-host,id=asix,bus=ehci.0,vendorid=0x0b95,productid=0x7720 &
+sudo qemu-system-$QARCH -enable-kvm -drive format=raw,file=disk-$DISTRO-$CNAME-$IARCH-$DSIZE.img -cdrom $DISO -boot c -m 512 -monitor telnet:localhost:$TPORT,server,nowait & #-device usb-ehci,id=ehci -device usb-host,id=asix,bus=ehci.0,vendorid=0x0b95,productid=0x7720 &
 sleep 10
 ./host2guest.sh $QCMPAYLOAD $TPORT $IARCH
 #sleep wait or continue after host2guest finish? 
 
 #convert current image to vbox appliance
 #VirtualBox VM NAME
-OVMN=$DISTRO-$IARCH-$DSIZE
-VBoxManage convertfromraw disk-$DISTRO-$IARCH-$DSIZE.img --format vdi $OVMN.vdi
+OVMN=$DISTRO-$CNAME-$IARCH-$DSIZE
+VBoxManage convertfromraw disk-$DISTRO-$CNAME-$IARCH-$DSIZE.img --format vdi $OVMN.vdi
 #modifymedium vdi --resize mb ?
 #modifyhds ?
 VBoxManage createvm --name $OVMN --ostype "$OVBOSTYPE" --register #32bit/Debian_64 #VBoxManage list ostypes
