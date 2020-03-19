2cda6bdee96a7ac95dcdb4d025e0d88cc69b30d8
Walter Sonius <walterav1984@gmail.com>
2020-03-19 21:50:52 +0100

    Update rf.sh ubuntu 20.04 unhide grub2 menu

--- rf.sh
+++ rf.sh
@@ -44,6 +44,8 @@ sudo sed -i "s/do_symlinks = yes/do_symlinks = no/g" /etc/kernel-img.conf
 }
 
 function grub2defaults {
+sudo sed -i 's/GRUB_TIMEOUT_STYLE=hidden/#GRUB_TIMEOUT_STYLE=hidden/g' /etc/default/grub
+sudo sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=5/g' /etc/default/grub
 sudo sed -i 's/GRUB_HIDDEN_TIMEOUT=0/#GRUB_HIDDEN_TIMEOUT=0/g' /etc/default/grub
 sudo sed -i 's/=console/="console serial"/g' /etc/default/grub
 sudo sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' /etc/default/grub
